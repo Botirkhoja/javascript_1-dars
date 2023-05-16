68b93c7d99a2c2d9b391aed799e07d118781430d
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-16 18:02:42 +0300

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,38 @@
+// alert("Websitega xush kelibsiz");
+
+// prompt("Bot emasligingizni isbotlang");
+
+// confirm("Salom");
+
+// console.log("Assalomu aleykum");
+
+// console.log("Nima Gap");
+
+// console.log("Yunus");
+
+
+let ism = "Yunus"
+
+let familya = "Obidxojayev"
+
+let email = "obidxojayevbotir@gmail.com"
+
+let tel = +998909969887
+
+let yashash = "Uyda"
+
+let manzil = "Qo'shnimmi yonida"
+
+console.log(ism);
+
+console.log(familya);
+
+console.log(email);
+
+console.log(tel);
+
+console.log(yashash);
+
+console.log(manzil);
+
+console.log(ism, familya, email, tel, yashash, manzil)
